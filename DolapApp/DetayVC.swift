48ff991ad805b88ee7aa6e5c25e2267855cf45a5
melek <melekturudi3@gmail.com>
2022-09-01 20:20:09 +0300

    added 4 image for story button, scrollview, 1 favorite button and 1 label

---
+++ DolapApp/DetayVC.swift
@@ -0,0 +1,21 @@
+//
+//  DetayVC.swift
+//  DolapApp
+//
+//  Created by melek türüdi on 1.09.2022.
+//
+
+import UIKit
+
+class DetayVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
